8f7cf8e28639901488e00e2ac16fb0562ef4a566
NooaV-M <nooa.vainio-mattila@metropolia.fi>
2024-10-09 21:07:29 +0300

    laser turret and better turret targeting added

--- turret.py
+++ turret.py
@@ -14,6 +14,10 @@ class Turret(pg.sprite.Sprite):
         self.selected = False
         self.target = None
         self.laser_level = 1
+        self.laser_level_progress = 0
+        self.firing = False
+        self.laser_damage = 2
+
 
         #position variables
         self.tile_x = tile_x
@@ -58,36 +62,46 @@ class Turret(pg.sprite.Sprite):
         return animation_list
     
     def update(self, enemy_group):
-        #if target picked, play firing animation
-        if self.target:
+        #if firing, play firing animation
+        if self.firing:
             self.play_animation()
         else:
-            #search for new target after cooldown
+            #fire again after cooldown
             if pg.time.get_ticks() - self.last_shot > self.cooldown:
+                # search for new target if needed
                 self.pick_target(enemy_group)
+                if self.target is not None:
+                    self.firing = True
+                    #damage and sfx
+                    self.damage_target()
+
 
 
     def pick_target(self, enemy_group):
-        #find an enemy to target
-        x_dist = 0
-        y_dist = 0
-        #loop through enemies and find the closest one within range
-        for enemy in enemy_group:
-            if enemy. health > 0:
-                x_dist = enemy.pos[0] - self.x
-                y_dist = enemy.pos[1] - self.y
-                dist = math.sqrt(x_dist ** 2 + y_dist ** 2)
-                if dist < self.range:
-                    self.target = enemy
-                    self.angle = math.degrees(math.atan2(-y_dist, x_dist))
-                    #check turret type and damage enemy
-                    if self.cooldown == 0:
-                        self.target.health -= self.damage*self.laser_level*4
-                    else:
-                        self.target.health -= self.damage
-                    #play sound fx
-                    self.shot_fx.play()
-                    break
+        if self.target is None or self.target not in enemy_group:
+            #decrease lasers level when its target dies
+            self.laser_level = 1
+            self.target = None
+            #find an enemy to target
+            x_dist = 0
+            y_dist = 0
+            closest_distance = self.range
+            #loop through enemies and find the closest one within range
+            for enemy in enemy_group:
+                if enemy.health > 0:
+                    x_dist = enemy.pos[0] - self.x
+                    y_dist = enemy.pos[1] - self.y
+                    dist = math.sqrt(x_dist ** 2 + y_dist ** 2)
+                    #check if distance is shorter than last shortest and store
+                    if dist < closest_distance:
+                        closest_enemy = enemy
+                        closest_distance = dist
+            #check if stored distance is in range and assign target and angle to target
+            if closest_distance < self.range:
+                self.target = closest_enemy
+                self.angle = math.degrees(math.atan2(-(self.target.pos[1] - self.y), self.target.pos[0] - self.x))
+        else:
+            self.angle = math.degrees(math.atan2(-(self.target.pos[1] - self.y), self.target.pos[0] - self.x))
 
     def play_animation(self):
         #update image
@@ -96,14 +110,36 @@ class Turret(pg.sprite.Sprite):
         if pg.time.get_ticks() - self.update_time > c.ANIMATION_DELAY:
             self.update_time = pg.time.get_ticks()
             self.frame_index += 1
-            
+
+            #check if animation ended
             if self.frame_index >= len(self.animation_list):
                 self.frame_index = 0
-                #record completed time and clear target so cooldown can begin
+                #record completed time and end firing so cooldown can begin
                 self.last_shot = pg.time.get_ticks()
-                self.target = None
+                self.firing = False
 
+                #clear target if target out of range
+                x_dist = self.target.pos[0] - self.x
+                y_dist = self.target.pos[1] - self.y
+                dist = math.sqrt(x_dist ** 2 + y_dist ** 2)
+                if dist > self.range:
+                    self.target = None
+                    print("out of range")
 
+    def damage_target(self):
+        # check turret type and damage enemy
+        if self.cooldown == 0:
+            self.target.health -= self.damage * self.laser_damage * 2**(self.laser_level -1)
+        else:
+            self.target.health -= self.damage
+        #increase laser level
+        if self.laser_level_progress <= 15:
+            self.laser_level_progress += 1
+        if self.laser_level_progress >= 15 and self.laser_level <= 3:
+            self.laser_level += 1
+            self.laser_level_progress = 0
+        # play sound fx
+        self.shot_fx.play()
     
     def draw(self, surface):
         #Check if base turret or laser and rotate image if laser
@@ -120,15 +156,8 @@ class Turret(pg.sprite.Sprite):
 
         # draw laser turret laser
         if self.cooldown == 0 and self.target != None:
-            pg.draw.line(surface, "red", (self.x, self.y), (self.target.pos[0], self.target.pos[1]), 5)
-        #draw laser level
-        if self.cooldown == 0:
-            colors = {
-                1 : "azure",
-                2 : "blue",
-                3 : "orange",
-            }
-            pg.draw.rect(surface,  colors[self.laser_level],(self.x,self.y+30,20,20))
+            pg.draw.line(surface, "red", (self.x, self.y), (self.target.pos[0], self.target.pos[1]), self.laser_level**2)
+
             #draw target indicator
-            if self.target:
-                pg.draw.rect(surface, colors[self.laser_level],(self.target.pos[0],self.target.pos[1]+30,20,20))
+        if self.target:
+            pg.draw.rect(surface, "blue",(self.target.pos[0],self.target.pos[1]+30,20,20))
